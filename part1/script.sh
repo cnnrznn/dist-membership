cb78b2dc91ff46f6447ad502f50a49fb9cefc725
Connor Zanin <zanin@login-students.ccs.neu.edu>
2018-10-30 05:55:38 -0400

    working login/command script for part 1 - still need to insert timing gaps and log output

--- part1/script.sh
+++ part1/script.sh
@@ -4,13 +4,13 @@ SESSION=$USER
 
 tmux -2 new-session -d -s $SESSION
 
-for i in $(seq 0 9)
+for i in $(seq 10)
 do
-        tmux new-window -t $SESSION:$i -n $i \
-                "ssh -A $USER@vdi-linux-0$((50+$i)).ccs.neu.edu \
-                ./prj2_tm -h hostfile -p 10000 -i $i"
+        tmux new-window -n $i 
+        tmux send-keys -t $SESSION:$i "ssh vdi-linux-0$((29+$i)).ccs.neu.edu" Enter
+        tmux send-keys -t $SESSION:$i "cd src/dist-membership" Enter
+        tmux send-keys -t $SESSION:$i "./prj2 -h hostfile -p 10000 -i $(($i-1))" Enter
 done
 
 tmux attach-session -t $SESSION
 
-tmux kill-session -t $SESSION
